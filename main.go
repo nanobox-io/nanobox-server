98ae5431796ccc4962f2b6d3169c20217619aaf1
sdomino <sdomino@pagodabox.com>
2014-10-29 17:14:44 -0600

    code review

--- main.go
+++ main.go
@@ -22,3 +22,27 @@ func main() {
 	}
 	fmt.Println(configMap)
 }
+
+// func main() {
+
+//   // command line args w/o program
+//   args := os.Args[1:]
+
+//   // set default config file
+//   configFile := "default.conf"
+
+//   // override default if config file provided
+//   if len(args) >= 1 {
+//     configFile = args[0]
+//   }
+
+//   // parse config file
+//   configMap, err := parseConfig(configFile)
+//   if err != nil {
+//     fmt.Printf("Unable to parse config file: %s", err)
+//     os.Exit(1)
+//   }
+
+//   // success!
+//   fmt.Println(configMap)
+// }
