53482467bd3d2c57112ecda8160c4289629d59a7
Lyon Hill <lyondhill@gmail.com>
2015-07-28 14:07:38 -0600

    when the Service environment is established it will also create a tunnel for any services

--- jobs/startup.go
+++ jobs/startup.go
@@ -38,4 +38,18 @@ func (j *Startup) Process() {
 		config.Router.Handler = router.NoDeploy{}
 	}
 
+
+	// we also need to set up a ssh tunnel for each running docker container
+	// this is easiest to do by creating a ServiceEnv job and working it
+	worker := util.NewWorker()
+	worker.Blocking = true
+	worker.Concurrent = true
+
+	serviceContainers, _ = util.ListContainers("service")
+	for _, container := range serviceContainers {
+		s := ServiceEnv{UID: container.Config.Labels["uid"]}
+		worker.Queue(&s)
+	}
+
+	worker.Process()
 }
