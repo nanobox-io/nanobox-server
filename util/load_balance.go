0921a1559fe8a19e03078def90a56c938041b555
Lyon Hill <lyondhill@gmail.com>
2015-09-08 13:48:39 -0600

    add the load balancer functionality

---
+++ util/load_balance.go
@@ -0,0 +1,58 @@
+package util
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/pagodabox/nanobox-lvs"
+	"github.com/pagodabox/nanobox-server/config"
+)
+
+// make sure the router is being forwarded
+func init() {
+	AddForward("80", config.IP, config.Ports["router"])
+}
+
+// add a server into the lvs system
+func AddForward(fromPort, toIp, toPort string) error {
+	fromInt, err := strconv.Atoi(fromPort)
+	if err != nil {
+		return err
+	}
+	_, err = lvs.AddVip(config.IP, fromInt)
+	if err != nil {
+		return err
+	}
+	toInt, _ := strconv.Atoi(toPort)
+	_, err = lvs.AddServer(fmt.Sprintf("%s:%d", config.IP, fromInt), toIp, toInt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func RemoveForward(ip string) error {
+	vips, err := lvs.ListVips()
+	if err != nil {
+		return err
+	}
+
+	errorString := ""
+
+	for _, vip := range vips {
+		for _, server := range vip.Servers {
+			if server.Host == ip {
+				err := lvs.DeleteVip(fmt.Sprintf("%s:%d", vip.Host, vip.Port))
+				if err != nil {
+					errorString = fmt.Sprintf("%s%v\n", errorString, err.Error())
+				}
+				break
+			}
+		}
+	}
+
+	if errorString != "" {
+		return fmt.Errorf(errorString)
+	}
+	return nil
+}
