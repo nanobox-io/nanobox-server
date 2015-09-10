7f6c088f0da9434cdab84cf876ce8df50620041a
Lyon Hill <lyondhill@gmail.com>
2015-09-10 09:26:07 -0600

    comment the routes until they are ready

--- jobs/routes.go
+++ jobs/routes.go
@@ -1,25 +1,25 @@
 package jobs
 
-import (
-	"github.com/pagodabox/nanobox-boxfile"
-	"github.com/pagodabox/nanobox-router"
-)
+// import (
+// 	"github.com/pagodabox/nanobox-boxfile"
+// 	"github.com/pagodabox/nanobox-router"
+// )
 
 
-func configureRoutes(box boxfile.Boxfile) {
-	webs := box.Nodes("web")
-	for _, web := range webs {
-		b := box.Node(web)
-		if ports, ok := b.Value("ports").([]string); ok
+// func configureRoutes(box boxfile.Boxfile) {
+// 	webs := box.Nodes("web")
+// 	for _, web := range webs {
+// 		b := box.Node(web)
+// 		if ports, ok := b.Value("ports").([]string); ok
 		
-	}
+// 	}
 
-}
+// }
 
-func ports(box boxfile.Boxfile) []string {
-	ports, ok := b.Value("ports").([]string)
-	if ok {
-		return ports
-	}
-	return []string
-}
+// func ports(box boxfile.Boxfile) []string {
+// 	ports, ok := b.Value("ports").([]string)
+// 	if ok {
+// 		return ports
+// 	}
+// 	return []string
+// }
