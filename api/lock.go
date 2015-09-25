aaf6ec9e078fc381e88d8a5a87416a6c78d24cc6
Lyon Hill <lyondhill@gmail.com>
2015-09-25 16:42:36 -0600

    add a lock system

---
+++ api/lock.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2014 Pagoda Box Inc.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
+
+package api
+
+import (
+	"net/http"
+	"fmt"
+
+	"github.com/pagodabox/nanobox-server/util"
+)
+
+func (api *API) Suspend(rw http.ResponseWriter, req *http.Request) {
+	if util.LockCount() <= 0 {
+		return
+	}
+
+	writeBody(map[string]string{"error": fmt.Sprintf("Current lock count: %d", util.LockCount())}, rw, http.StatusNotAcceptable)
+}
+
+
+func (api *API) LockCount(rw http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(rw, "%d", util.LockCount())
+}
+
+// keeps a lock open as long as the connection is established with 
+// my service
+func (api *API) Lock(rw http.ResponseWriter, req *http.Request) {
+	util.Lock()
+	defer util.Unlock()
+
+	cNotify := rw.(http.CloseNotifier)
+	<-cNotify.CloseNotify()
+}
---
+++ util/lock.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"sync/atomic"
+)
+
+var lockCount = int64(0)
+
+func LockCount() int {
+	return int(lockCount)
+}
+
+func Lock() {
+	atomic.AddInt64(&lockCount, 1)
+}
+
+func Unlock() {
+	atomic.AddInt64(&lockCount, -1)	
+}
+
