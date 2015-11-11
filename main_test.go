ebd37a1716354d4a4c89363f1f013433acb1257d
Lyon Hill <lyondhill@gmail.com>
2015-11-11 15:27:23 -0700

    crank up the logging

--- main_test.go
+++ main_test.go
@@ -26,7 +26,7 @@ import (
 var apiClient = api.Init()
 
 func TestMain(m *testing.M) {
-	config.Log = lumber.NewConsoleLogger(lumber.ERROR)
+	config.Log = lumber.NewConsoleLogger(lumber.DEBUG)
 
 	curDir, err := os.Getwd()
 	if err != nil {
